7e69936bbf40fc31fffc9f2e8964b5495146e0b3
ilvalamberte <38037680+ilvalamberte@users.noreply.github.com>
2019-10-04 15:16:12 +0200

    Add files via upload

--- scroll_code/script.js
+++ scroll_code/script.js
@@ -1,40 +1,29 @@
 
 
-// The debounce function receives our function as a parameter
-const debounce = (fn) => {
+let topOfSite = document.getElementById("top-of-site-pixel-anchor");
 
-    // This holds the requestAnimationFrame reference, so we can cancel it if we wish
-    let frame;
-  
-    // The debounce function returns a new function that can receive a variable number of arguments
-    return (...params) => {
-      
-      // If the frame variable has been defined, clear it now, and queue for next frame
-      if (frame) { 
-        cancelAnimationFrame(frame);
-      }
-  
-      // Queue our function call for the next frame
-      frame = requestAnimationFrame(() => {
-        
-        // Call our function and pass any params we received
-        fn(...params);
-      });
-  
-    } 
-  };
-  
-  
-  // Reads out the scroll position and stores it in the data attribute
-  // so we can use it in our stylesheets
-  const storeScroll = () => {
-    document.documentElement.dataset.scroll = window.scrollY;
+
+
+
+
+function observeScroll() {
+if (
+  "IntersectionObserver" in window &&
+  "IntersectionObserverEntry" in window &&
+  "intersectionRatio" in window.IntersectionObserverEntry.prototype
+) {
+let observer = new IntersectionObserver(entries => {
+  if (entries[0].boundingClientRect.y < 0) {
+    document.body.classList.add("header-not-at-top");
+  } else {
+    document.body.classList.remove("header-not-at-top");
   }
-  
-  // Listen for new scroll events, here we debounce our `storeScroll` function
-  document.addEventListener('scroll', debounce(storeScroll), { passive: true });
-  
-  // Update scroll position for first time
-  storeScroll();
-  
-  
+});
+observer.observe(topOfSite);
+}
+
+}
+
+
+
+observeScroll();
